8fad9b4ce9341f0cf2ff259adc756fe2309bb331
Groophy Lifefor <77299279+GroophyLifefor@users.noreply.github.com>
2023-05-13 15:14:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,7 +17,35 @@ function sec() {
     }
 
     let total = parties.er + parties.ko + parties.si;
-    $('#er').html(String((parties.er / total) * 100).slice(0, 5) + '%<br>count: ' + parties.er)
-    $('#ko').html(String((parties.ko / total) * 100).slice(0, 5) + '%<br>count: ' + parties.ko)
-    $('#si').html(String((parties.si / total) * 100).slice(0, 5) + '%<br>count: ' + parties.si)
+    $('#er').html(String((parties.er / total) * 100).slice(0, 5) + `%<br>count: ` + parties.er)
+    $('#ko').html(String((parties.ko / total) * 100).slice(0, 5) + `%<br><div class='ko'/><br>count: ` + parties.ko)
+    $('#si').html(String((parties.si / total) * 100).slice(0, 5) + `%<br><div class='si'/><br>count: ` + parties.si)
+
+    var xValues = ["Erdoğan", "Kılıçdaroğlu", "Oğan"];
+    var yValues = [(parties.er / total) * 100, (parties.ko / total) * 100, (parties.si / total) * 100];
+    var barColors = ["red", "green", "blue"];
+
+    new Chart("myChart", {
+        type: "bar",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+            }]
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                y: {
+                    suggestedMin: 0,
+                    suggestedMax: 100
+                }
+            },
+            title: {
+                display: true,
+                text: "2023 Cumhurbaşkanlığı Aday Seçimi"
+            }
+        }
+    });
 }
